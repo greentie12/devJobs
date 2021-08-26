68f7ab2fb7a2141f97579cd1d42f6dd9b84f84c5
greentie12 <tdominguez@fordham.edu>
2021-08-26 10:39:33 -0400

    pagination added

--- js/script.js
+++ js/script.js
@@ -1,61 +1,85 @@
 const gallery = document.querySelector(".gallery");
+const loadMore = document.querySelector(".load-more");
 
 const file = "../json/data.json";
 
-const JobItem = {
+const jobItems = {
   jobsPerPage: 12,
   currentPage: 1,
+  maxPage: null,
 };
 
 async function retrieveJobs(file) {
   const response = await fetch(file);
   const jobData = await response.json();
 
-  console.log(jobData.length);
+  jobItems.maxPage = Math.ceil(jobData.length / jobItems.jobsPerPage);
 
-  appendJobs(jobData);
+  appendJobs(jobData, jobItems.currentPage);
 }
 
 retrieveJobs(file);
 
-const appendJobs = (jobs) => {
-  jobs.forEach((job) => {
-    const {
-      logo,
-      logoBackground,
-      postedAt,
-      contract,
-      position,
-      company,
-      location,
-    } = job;
+const appendJobs = (jobs, page) => {
+  gallery.innerHTML = "";
+  jobItems.currentPage = page;
+  let startIndex = (jobItems.currentPage - 1) * jobItems.jobsPerPage;
+  let endIndex;
 
-    const jobCard = document.createElement("div");
+  if (startIndex + jobItems.jobsPerPage > jobs.length) {
+    endIndex = jobs.length;
+  } else {
+    endIndex = startIndex + jobItems.jobsPerPage;
+  }
+
+  for (let x = startIndex; x < endIndex; x++) {
+    let jobDataList = jobs[x];
+    let job = {
+      logo: jobDataList.logo,
+      logoBackground: jobDataList.logoBackground,
+      postedAt: jobDataList.postedAt,
+      contract: jobDataList.contract,
+      position: jobDataList.position,
+      company: jobDataList.company,
+      location: jobDataList.location,
+    };
+
+    let jobCard = document.createElement("div");
     jobCard.classList.add("card");
 
     jobCard.insertAdjacentHTML(
       "beforeend",
       `
-	<div style="background-color:${logoBackground}" class="logo-div">
-		<img src="${logo}" alt="" />
+	<div style="background-color:${job.logoBackground}" class="logo-div">
+		<img src="${job.logo}" alt="" />
 	</div>
 	<div class="info-container">
 		<div class="card-info">
 	  		<div class="posted-time">
-				<p class="posted">${postedAt}</p>
+				<p class="posted">${job.postedAt}</p>
 				<div class="bullet">&#8226;</div>
-				<p class="time">${contract}</p>
+				<p class="time">${job.contract}</p>
 	 		 </div>
-	  		<h3 class="job-title">${position}</h3>
-	  		<p class="company">${company}</p>
+	  		<h3 class="job-title">${job.position}</h3>
+	  		<p class="company">${job.company}</p>
 		</div>
 		<div class="location">
-	  		<h4>${location}</h4>
+	  		<h4>${job.location}</h4>
 		</div>
   	</div>
 	`
     );
 
     gallery.appendChild(jobCard);
-  });
+  }
 };
+
+loadMore.addEventListener("click", function () {
+  if (jobItems.currentPage >= jobItems.maxPage) {
+    jobItems.currentPage = 1;
+  } else {
+    jobItems.currentPage++;
+  }
+
+  retrieveJobs(file);
+});
